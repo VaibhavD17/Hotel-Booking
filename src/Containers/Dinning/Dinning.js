c3544c27f74420e43e4ab8db61713183a051186e
Vaibhav Gohil <vaibhav9723142179@gmai.com>
2025-06-04 10:54:56 +0530

    04-06-25

--- src/Containers/Dinning/Dinning.js
+++ src/Containers/Dinning/Dinning.js
@@ -5,6 +5,14 @@ import hotelexp2 from "../../assets/image/hotel-exp2.jpg"
 import hotelexp3 from "../../assets/image/hotel-exp3.jpg"
 import dinningImg from '../../assets/image/dinning-group.png'
 import craftedcuisine from '../../assets/image/crafted-cuisine.jpg'
+import restoimg1 from '../../assets/image/resto-img-1.jpg'
+import restoimg2 from '../../assets/image/resto-img-2.jpg'
+import restoimg3 from '../../assets/image/resto-img-3.jpg'
+import restoimg4 from '../../assets/image/resto-img-4.jpg'
+import restoimg5 from '../../assets/image/resto-img-5.jpg'
+import restoimg6 from '../../assets/image/resto-img-6.jpg'
+import restoimg7 from '../../assets/image/resto-img-7.jpg'
+import restoimg8 from '../../assets/image/resto-img-8.jpg'
 
 
 function Dinning() {
@@ -38,7 +46,7 @@ function Dinning() {
                             <h6 className="sub-title-1">Hotel Experience</h6>
                             <h4 className="main-title-1">From Farm to Fork: Enjoy Fresh, Seasonal Dishes at Bokinn</h4>
                             <p className="sub-title-2">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</p>
-                            
+
                             <div className="opning-box">
                                 <div className="opnning-time">
                                     <h5>Reservation Phone</h5>
@@ -64,7 +72,7 @@ function Dinning() {
                             <h4 className="main-title-1">Dine. Delight. Discover.</h4>
                             <p className="sub-title-2">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud  ullamco.</p>
                             <p className="sub-title-2">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
-                    
+
                         </div>
                         <div className='img-fluid'>
                             <img className='width600' src={dinningImg}></img>
@@ -75,19 +83,123 @@ function Dinning() {
             </section>
             <section id="crafted">
                 <div className="crafted-box">
-                        <h4 className="main-title-1 text-center">Crafted Cuisine</h4>
+                    <h4 className="main-title-1 text-center">Crafted Cuisine</h4>
 
-                        <div className="crafted-cuisine">
-                            <img src={craftedcuisine} />
-                            <div className="bestseller crafted-cuisine-data">
-                                <h5>Bestseller</h5>
-                                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</p>
-                            </div>
-                            <div className="taste crafted-cuisine-data">
-                                <h5>Taste the Perfection</h5>
-                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</p>
+                    <div className="crafted-cuisine">
+                        <img src={craftedcuisine} />
+                        <div className="bestseller crafted-cuisine-data">
+                            <h5>Bestseller</h5>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</p>
+                        </div>
+                        <div className="taste crafted-cuisine-data">
+                            <h5>Taste the Perfection</h5>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</p>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <section id="restaurant-gallery">
+                <div className="restaurant-gallery-data">
+                <div className="our-rooms-start">
+                    <h4>Our Restaurant Gallery</h4>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                </div>
+                <div className="rest-gal-box">
+                    <div className="restorant-img-box col-xl-12">
+                        <img className="col-xl-4 " src={restoimg1} />
+                        <img className="col-xl-3" src={restoimg2} />
+                        <img className="col-xl-3" src={restoimg3} />
+                        <img className="col-xl-2" src={restoimg4} />
+                    </div>
+                    <div className="restorant-img-box col-xl-12">
+                        <img className="col-xl-2" src={restoimg5} />
+                        <img className="col-xl-3" src={restoimg6} />
+                        <img className="col-xl-3" src={restoimg7} />
+                        <img className="col-xl-4" src={restoimg8} />
+                    </div>
+                </div>
+                </div>
+            </section>
+            <section id="Restaurant-Menu">
+                <div className="res-menu-box">
+                    <div className="our-rooms-start">
+                        <h4>Restaurant Menu</h4>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                    </div>
+                    <div className="menu-category-box">
+                        <ul>
+                            <li><a className="menu-category">Small Plates</a></li>
+                            <li><a className="menu-category">Cold Appetizers</a></li>
+                            <li><a className="menu-category">Vegetarian & Vegan</a></li>
+                            <li><a className="menu-category">Burgers & Sandwiches</a></li>
+                            <li><a className="menu-category">Dips & Spreads</a></li>
+                            <li><a className="menu-category">Seafood Specialties</a></li>
+                            <li><a className="menu-category">Chinese Food</a></li>
+                            <li><a className="menu-category">Beverages</a></li>
+                        </ul>
+                    </div>
+                    <div className="menu-item-box">
+                        <div className="menu-item">
+                            <img src={restoimg1} />
+                            <div className="menu-item-data">
+                                <div className="menu-name">
+                                    <h5>Tuscan Herb-Crusted Chicken</h5>
+                                    <h4>$123</h4>
+                                </div>
+                                <p>Salted caramel, Puff pastry, salad</p>
                             </div>
                         </div>
+                        <div className="menu-item">
+                            <img src={restoimg2} />
+                            <div className="menu-item-data">
+                                <div className="menu-name">
+                                    <h5>Tuscan Herb-Crusted Chicken</h5>
+                                    <h4>$123</h4>
+                                </div>
+                                <p>Salted caramel, Puff pastry, salad</p>
+                            </div>
+                        </div>
+                        <div className="menu-item">
+                            <img src={restoimg3} />
+                            <div className="menu-item-data">
+                                <div className="menu-name">
+                                    <h5>Tuscan Herb-Crusted Chicken</h5>
+                                    <h4>$123</h4>
+                                </div>
+                                <p>Salted caramel, Puff pastry, salad</p>
+                            </div>
+                        </div>
+                        <div className="menu-item">
+                            <img src={restoimg4} />
+                            <div className="menu-item-data">
+                                <div className="menu-name">
+                                    <h5>Tuscan Herb-Crusted Chicken</h5>
+                                    <h4>$123</h4>
+                                </div>
+                                <p>Salted caramel, Puff pastry, salad</p>
+                            </div>
+                        </div>
+                        <div className="menu-item">
+                            <img src={restoimg5} />
+                            <div className="menu-item-data">
+                                <div className="menu-name">
+                                    <h5>Tuscan Herb-Crusted Chicken</h5>
+                                    <h4>$123</h4>
+                                </div>
+                                <p>Salted caramel, Puff pastry, salad</p>
+                            </div>
+                        </div>
+                        <div className="menu-item">
+                            <img src={restoimg6} />
+                            <div className="menu-item-data">
+                                <div className="menu-name">
+                                    <h5>Tuscan Herb-Crusted Chicken</h5>
+                                    <h4>$123</h4>
+                                </div>
+                                <p>Salted caramel, Puff pastry, salad</p>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </section>
         </div>
